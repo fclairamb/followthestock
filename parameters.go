54273f0eca00dc194a936c0434816816f2330510
Florent Clairambault <florent@clairambault.fr>
2014-03-29 23:03:43 +0100

    Removing (and changing) password to prepare open-source release.

--- parameters.go
+++ parameters.go
@@ -20,7 +20,7 @@ func ParametersParse(par *Parameters) {
 	flag.StringVar(&par.dbfile, "dbfile", "followthestock.db", "database file")
 	flag.StringVar(&par.server, "server", "talk.google.com:443", "XMPP Server")
 	flag.StringVar(&par.username, "username", "followthestock@gmail.com", "XMPP Username")
-	flag.StringVar(&par.password, "password", "SuperStock", "XMPP Password")
+	flag.StringVar(&par.password, "password", "", "XMPP Password")
 	flag.BoolVar(&par.notls, "notls", false, "Disable TLS")
 	flag.BoolVar(&par.debug, "debug", false, "Enable debugging")
 	flag.BoolVar(&par.exact, "exact", false, "Exact timing")
